65a738b4dec7fa9460c49c8aaca27e3bf4aa6cbb
jananpatel2002 <jkpatel2001@student.stcc.edu>
2022-10-29 14:14:42 -0400

    Finished project. Can create a hirst like painting

---
+++ main.py
@@ -0,0 +1,36 @@
+###This code will not work in repl.it as there is no access to the colorgram package here.###
+##We talk about this in the video tutorials##
+import colorgram
+import turtle
+import random
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+
+# Adding color tuples to a list
+for color in colors:
+    rgb_colors.append(color.rgb)
+
+turtle.colormode(255)
+timmy = turtle.Turtle()
+
+# Filling colors and circles
+timmy.penup()
+timmy.setpos(-225, -225)
+x_coordinate = timmy.xcor()
+timmy.speed(0)
+timmy.hideturtle()
+for _ in range(10):
+    print(timmy.pos())
+    for _ in range(10):
+        color = (random.choice(rgb_colors))
+        timmy.fillcolor(color)
+        timmy.begin_fill()
+        timmy.circle(20)
+        timmy.end_fill()
+        timmy.forward(50)
+    timmy.setpos(x_coordinate, (timmy.ycor() + 50.0))
+
+
+screen = turtle.Screen()
+screen.exitonclick()
